f9c9612474a077378a75ddb451ae75a47cd4aa38
Arumparithi42 <2024503542@student.annauniv.edu>
2026-07-22 14:32:29 +0530

    Completed Doubly Linked List

--- DoublyLinkedList/DLL.java
+++ DoublyLinkedList/DLL.java
@@ -56,10 +56,23 @@ public class DLL{
             temp = temp.next;
         temp.next = newNode;
         newNode.prev = temp;
+        tail = newNode;
         size++;
     }
     // Insert
     public void insert(int index, int value){
+        if(index > size){
+            System.out.println("Invalid index");
+            return;
+        }
+        if(index == 0){
+            insertFirst(value);
+            return;
+        }
+        if(index == size){
+            insertLast(value);
+            return;
+        }
         Node temp = head;
         for(int i = 0; i < index-1; i++)
             temp = temp.next;
@@ -69,19 +82,76 @@ public class DLL{
         temp.next.prev = newNode;
         temp.next = newNode;
         newNode.prev = temp;
+        size++;
+    }
+    // Delete First
+    public void deleteFirst(){
+        if(head == null)
+            return;
+        head = head.next;
+        if(head != null)
+            head.prev = null;
+        else
+            tail = null;
+        size--;
+    }
+    // Delete Last
+    public void deleteLast(){
+        if(head == null){
+            return;
+        }
+        
+        if(head.next == null){
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+        Node temp = head;
+        while(temp.next.next != null){
+            temp = temp.next;
+        }
+        temp.next = null;
+        tail = temp;
+        size--;
+    }
+    public void delete(int index){
+        if(index < 0 || index > size-1){
+            System.out.println("Invlid index");
+            return;
+        }
+        if(index == 0){
+            deleteFirst();
+            return;
+        }
+        if(index == size - 1){
+            deleteLast();
+            return;
+        }
+
+        Node temp = head;
+        for(int i = 0; i < index - 1; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        temp.next.prev = temp;
+        size--;
     }
 
     public static void main(String[] args) {
         DLL list = new DLL();
         list.insertLast(3);
         list.insertFirst(2);
-        list.diplay();
+        //list.diplay();
         list.insert(1,4);
-        list.diplay();
+        // list.diplay();
         list.insert(1,5);
+        
+        list.insert(0,6);
+        
         list.diplay();
-        list.insert(3,6);
+        list.delete(3);
         list.diplay();
-        list.diplayReverse();
+        System.out.print("reverse : ");list.diplayReverse();
     }
 }
